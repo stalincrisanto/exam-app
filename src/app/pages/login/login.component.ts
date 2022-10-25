9a6a1e8dec5399902c3c7dee513fbc62b634ac62
stalincrisanto <stalincrisanto@hotmail.com>
2022-10-24 23:58:30 -0500

    feat: add login methods

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData = {
+    userName : '',
+    password: ''
+  }
+
+  constructor(private snack: MatSnackBar, private login: LoginService) { }
 
   ngOnInit(): void {
+  }
+
+  formSubmit(){
+    if(this.loginData.userName.trim()===''|| this.loginData.userName===null){
+      this.snack.open('Username is required','',{
+        duration: 3000
+      })
+      return;
+    }
+    if(this.loginData.password.trim()===''|| this.loginData.password===null){
+      this.snack.open('password is required','',{
+        duration: 3000
+      })
+      return;
+    }
+    this.login.generateToken(this.loginData).subscribe((data: any) => {
+      console.log("toke--->",data);
+    },
+    (error) => {
+      console.log(error);
+    })
   }
 
 }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import baseUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+
+  //generate token
+  public generateToken (loginData: any){
+    return this.http.post(`${baseUrl}/generate-token`,loginData);
+  }
+
+  //login user: set token in localstorage
+  public loginUser(token: string){
+    localStorage.setItem('token',token);
+    return true;
+  }
+
+  public isLoggedIn(){
+    let tokenStorage = localStorage.getItem('token');
+    if(tokenStorage===undefined||tokenStorage===''||tokenStorage===null){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  //logout
+  public logout(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    return true;
+  }
+
+  public getToken(){
+    return localStorage.getItem('token');
+  }
+
+  public setUser(user:any){
+    localStorage.setItem('user',JSON.stringify(user));
+  }
+
+  public getUser(){
+    let userStorage = localStorage.getItem('user');
+    if(userStorage!==null){
+      return JSON.parse(userStorage);
+    } else {
+      this.logout();
+      return null;
+    }
+  }
+
+  public getUserRole(){
+    let user = this.getUser();
+    return user.authorities[0].authority;
+  }
+}
