faad964a471e58734637d714d545f3855985417f
stalincrisanto <stalincrisanto@hotmail.com>
2022-11-08 18:41:59 -0500

    feat: login process and routes protection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
+import { AdminGuard } from './services/admin.guard';
+import { NormalGuard } from './services/normal.guard';
 
 const routes: Routes = [
   {
@@ -20,6 +24,18 @@ const routes: Routes = [
     component: LoginComponent,
     pathMatch: 'full',
   },
+  {
+    path: 'admin',
+    component: DashboardComponent,
+    pathMatch: 'full',
+    canActivate: [AdminGuard]
+  },
+  {
+    path: 'user-dashboard',
+    component: UserDashboardComponent,
+    pathMatch: 'full',
+    canActivate: [NormalGuard]
+  }
 ];
 
 @NgModule({
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,7 @@ export class LoginComponent implements OnInit {
     password: ''
   }
 
-  constructor(private snack: MatSnackBar, private login: LoginService) { }
+  constructor(private snack: MatSnackBar, private login: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -40,11 +41,24 @@ export class LoginComponent implements OnInit {
 
       this.login.getCurrentUser().subscribe((user: any) => {
         this.login.setUser(user);
-        console.log(user);
+        if(this.login.getUserRole() == 'ADMIN'){
+          //admin dashboard
+          // window.location.href = '/admin';
+          this.router.navigate(['admin']);
+        } else if (this.login.getUserRole() == 'NORMAL') {
+          //normal user dashboard
+          // window.location.href = '/user-dashboard';
+          this.router.navigate(['user-dashboard']);
+        } else {
+          this.login.logout();
+        }
       })
     },
     (error) => {
       console.log(error);
+      this.snack.open("Invalid Details",'',{
+        duration: 3000
+      })
     })
   }
 
